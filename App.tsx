fbe745ce2bf1af1f906c27790249f05b259b721b
arnbie <arnau.tv2@gmail.com>
2024-11-21 10:40:53 +0100

    proba cursor

--- App.tsx
+++ App.tsx
@@ -23,7 +23,7 @@ const houses = [
   {
     id: 2,
   
-    title: 'Apartamento en el centr',
+    title: 'Apartamento en el centro de Madrid',
     description: 'Moderno apartamento en el corazón de la ciudad.',
     price: 200000,
     location: 'Madrid',
